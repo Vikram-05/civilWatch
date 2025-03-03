e72fdc6a17efdfd59b93fd5e769bed3999f7a8dc
Vikram-05 <vikramdbs9097@gmail.com>
2025-03-03 12:15:52 +0530

    Delete backend directory

--- backend/routes/creditRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import {giveCredit,getSimilarRep} from '../controllers/creditController.js'
-
-router.post('/', giveCredit);
-router.get('/getSimilarRep/:id', getSimilarRep);
-
-export default router
